dfc7930d621dce3345200e724c4352e57387bc31
mithunhalder123 <108678246+mithunhalder123@users.noreply.github.com>
2022-09-02 19:52:34 +0530

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,32 @@
+package restaurent;
+
+public class Bill {
+	private String fooditem;
+	private int qnt; //quantity
+	private double price;
+	public String getFooditem() {
+		return fooditem;
+	}
+	public void setFooditem(String fooditem) {
+		this.fooditem = fooditem;
+	}
+	public int getQnt() {
+		return qnt;
+	}
+	public void setQnt(int qnt) {
+		this.qnt = qnt;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Bill(String fooditem, int qnt, double price) {
+		super();
+		this.fooditem = fooditem;
+		this.qnt = qnt;
+		this.price = price;
+	}
+
+}
---
+++ Menu.java
@@ -0,0 +1,33 @@
+package restaurent;
+
+public class Menu {
+	private int id;
+	private String fooditem;
+	private float price;
+	
+	public Menu(int id, String fooditem, float price) {
+		super();
+		this.id = id;
+		this.fooditem = fooditem;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFooditem() {
+		return fooditem;
+	}
+	public void setFooditem(String fooditem) {
+		this.fooditem = fooditem;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+}
